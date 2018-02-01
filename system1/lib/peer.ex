b2590e4f38d0f4ab5999aeb6bec5551aafd290ae
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2018-02-01 19:33:52 +0000

    Finish system1

--- system1/lib/peer.ex
+++ system1/lib/peer.ex
@@ -16,25 +16,21 @@ defmodule Peer do
 	end
 
 	def broadcast(system, peer_index, neighbours, max_broadcast, received_status, sent_status, broadcast_counter) do
-		# If we have broadcast less than max_broadcast times, keep broadcasting
-		sent_status = if broadcast_counter < max_broadcast do
-			for neighbour <- neighbours, do: send neighbour, {:message, peer_index}
-			for elem <- sent_status, do: elem + 1
-		end || sent_status
-		
-		receive_message(system, peer_index, neighbours, max_broadcast, received_status, sent_status, broadcast_counter + 1)
-	end
-
-	def receive_message(system, peer_index, neighbours, max_broadcast, received_status, sent_status, broadcast_counter) do
 		receive do
 			# When receiving message, store it in the dict and then broadcast
 			{:message, sender} ->
-				received_status = List.update_at(received_status, sender, &(&1 + 1))
-				#IO.puts("Peer #{peer_index} has received message and its received counter is #{Enum.at(received_status, sender)}")
-				broadcast(system, peer_index, neighbours, max_broadcast, received_status, sent_status, broadcast_counter)
+				broadcast(system, peer_index, neighbours, max_broadcast, List.update_at(received_status, sender, &(&1 + 1)), sent_status, broadcast_counter)
 			:timeout ->
 				send system, {:done, peer_index, sent_status, received_status}
+			after 0 -> 
+				# If we have broadcast less than max_broadcast times, keep broadcasting
+				sent_status = if broadcast_counter < max_broadcast do
+					for neighbour <- neighbours, do: send neighbour, {:message, peer_index}
+					for elem <- sent_status, do: elem + 1
+				end || sent_status
+				broadcast(system, peer_index, neighbours, max_broadcast, received_status, sent_status, broadcast_counter + 1)
 		end
 	end
+
 end
 
--- system1/lib/system1.ex
+++ system1/lib/system1.ex
@@ -2,15 +2,22 @@ defmodule System1 do
   def main do
     num_of_peers = 5
     timeout = 3000
+    max_broadcast = 1000
 
     peers = 
-    for i <- 0..num_of_peers, do: spawn(Peer, :start, [self(), i])
-
-    for peer <- peers, do: Process.send_after(peer, :timeout, timeout)
+        for i <- 0..num_of_peers, do: Node.spawn(:'node#{i+1}@container#{i+1}.localdomain', Peer, :start, [self(), i])
+    
+    # peers = 
+    # for i <- 0..num_of_peers, do: spawn(Peer, :start, [self(), i])
 
     for peer <- peers, do: send peer, {:neighbours, peers}
 
-    for peer <- peers, do: send peer, {:broadcast, 1000}
+    for peer <- peers, do: send peer, {:broadcast, max_broadcast}
+
+    receive do
+      after timeout ->
+        for peer <- peers, do: send peer, :timeout
+    end
 
     listen(num_of_peers, 0)
     
@@ -20,7 +27,7 @@ defmodule System1 do
     receive do
       {:done, peer_index, sent_status, received_status} ->
         status = List.zip([sent_status, received_status])
-        IO.puts("Peer, S R:  #{peer_index} #{inspect status}")
+        IO.puts("#{peer_index}: #{inspect status}")
         #IO.inspect(status)
         if peer_counter < num_of_peers - 1 do
           listen(num_of_peers, peer_counter + 1)
