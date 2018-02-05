85dc8601f0bec848d0f5850723f7030f4af5077e
Tencho Tenev <tt1215@imperial.ac.uk>
2018-02-05 19:45:26 +0000

    Update lossy link with new usage, add RB module

--- system1/lib/common.ex
+++ system1/lib/common.ex
@@ -30,18 +30,37 @@ defmodule BEB do
 end
 
 defmodule LossyLink do
-  def run(id, reliability, app \\ {}, links \\ []) do
+  def run(reliability, app \\ {}, links \\ []) do
     receive do
-      {:bind_links, links} -> run(id, reliability, app, links)
-      {:bind_app, app} -> run(id, reliability, app, links)
+      {:bind_links, links} -> run(reliability, app, links)
+      {:bind_app, app} -> run(reliability, app, links)
       {:pl_send, msg, dest} ->
         if Enum.random(1..100) <= reliability do
           send Enum.at(links, dest), msg
         end
-        run(id, reliability, app, links)
+        run(reliability, app, links)
       msg ->
         send app, {:pl_deliver, msg}
-        run(id, reliability, app, links)
+        run(reliability, app, links)
+    end
+  end
+end
+
+defmodule RB do
+  def run(peers, beb, app \\ {}, delivered \\ []) do
+    receive do
+      {:bind_app, app} -> run(peers, beb, app)
+      {:rb_broadcast, msg} ->
+        send beb, {:beb_broadcast, msg}
+        run(peers, beb, app, delivered)
+      {:beb_deliver, msg} ->
+        if !Enum.member?(delivered, msg) do
+          send beb, {:beb_broadcast, msg}
+          send app, {:rb_deliver, msg}
+          run(peers, beb, app, [msg | delivered])
+        else
+          run(peers, beb, app, delivered)
+        end
     end
   end
 end
