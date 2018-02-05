69cbff0a057b70a6b7e4634159621d80d6cb0739
Tencho Tenev <tt1215@imperial.ac.uk>
2018-02-05 19:14:12 +0000

    Refactor system 5

--- system1/lib/system5/main.ex
+++ system1/lib/system5/main.ex
@@ -1,7 +1,7 @@
 defmodule System5 do
   def main(max_broadcast \\ 1000, n_peers \\ 5, timeout \\ 3000) do
     peer_ids = for i <- 0..n_peers-1 do
-      spawn System5.Peer, :run, [self(), i, n_peers, max_broadcast]
+      spawn System5.Peer, :run, [self(), i, n_peers, max_broadcast, timeout]
     end
 
     pl_ids = for i <- 0..n_peers-1 do
@@ -18,16 +18,11 @@ defmodule System5 do
       send p, {:ready}
     end
 
-    receive do
-    after timeout ->
-      for pl <- pl_ids do
-        send pl, {:stop, self()}
-      end
-    end
+    Process.sleep(timeout) 
 
     for i <- 0..n_peers-1 do
       receive do
-        {:done, ^i, sent_status, received_status} -> 
+        {:done, ^i, sent_status, received_status} ->
           status = List.zip([sent_status, received_status])
           IO.puts("#{i}: #{inspect status}")
         after 100 -> IO.puts("No reply from process #{i} in 100ms")
--- system1/lib/system5/peer.ex
+++ system1/lib/system5/peer.ex
@@ -1,46 +1,23 @@
 defmodule System5.Peer do
-  def run(system, peer_index, n_peers, max_broadcast) do
+  def run(system, peer_index, n_peers, max_broadcast, timeout) do
     peers = 0..n_peers-1
-    pl = spawn PerfectLink, :run, [peer_index]
-    beb = spawn BEB, :run, [peer_index, peers, pl]
+    reliability = 50
+    pl = spawn LossyLink, :run, [peer_index, reliability]
+    beb = spawn BEB, :run, [peers, pl]
     send system, {peer_index, pl}
     receive do
       {:ready} ->
-        initial_stats = List.duplicate(0, n_peers)
-        app = spawn System3.App, :run,
-          [peer_index, beb, initial_stats, initial_stats, max_broadcast]
+        app = spawn System3.App, :init,
+          [peer_index, system, beb, n_peers, max_broadcast, timeout]
         send beb, {:bind_app, app}
         send pl, {:bind_app, beb}
         if peer_index == 2 do
           receive do
-            after 5 -> 
+            after 5 ->
               Process.exit(app, :kill)
           end
         end
     end
-    
-  end
-end
 
-defmodule System5.App do
-
-  # Max broadcasts reached
-  def run(id, beb, received_status, sent_status, 0) do
-    receive do
-      {:beb_deliver, :stop, system} -> send system, {:done, id, sent_status, received_status}
-      {:beb_deliver, _, from} -> run(id, beb,
-        List.update_at(received_status, from, &(&1 + 1)), sent_status, 0)
-    end
-  end
-
-  def run(id, beb, received_status, sent_status, broadcast_left) do
-    receive do
-      {:beb_deliver, :stop, system} -> send system, {:done, id, sent_status, received_status}
-      {:beb_deliver, _, from} -> run(id, beb,
-        List.update_at(received_status, from, &(&1 + 1)), sent_status, broadcast_left)
-      after 0 ->
-        send beb, {:beb_broadcast, {}}
-        run(id, beb, received_status, Enum.map(sent_status, &(&1 + 1)), broadcast_left - 1)
-    end
   end
 end
