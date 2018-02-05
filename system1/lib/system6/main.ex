223da5ada1de5d22358bb033c96e8876be7d5dd6
Tencho Tenev <tt1215@imperial.ac.uk>
2018-02-05 19:45:52 +0000

    Complete system 6 implementation

--- system1/lib/system6/main.ex
+++ system1/lib/system6/main.ex
@@ -1,7 +1,7 @@
 defmodule System6 do
   def main(max_broadcast \\ 1000, n_peers \\ 5, timeout \\ 3000) do
     peer_ids = for i <- 0..n_peers-1 do
-      spawn System6.Peer, :run, [self(), i, n_peers, max_broadcast]
+      spawn System6.Peer, :run, [self(), i, n_peers, max_broadcast, timeout]
     end
 
     pl_ids = for i <- 0..n_peers-1 do
@@ -18,25 +18,15 @@ defmodule System6 do
       send p, {:ready}
     end
 
-    receive do
-    after timeout ->
-      for pl <- pl_ids do
-        send pl, {:stop, self()}
+    Process.sleep(timeout) 
+
+    for i <- 0..n_peers-1 do
+      receive do
+        {:done, ^i, sent_status, received_status} ->
+          status = List.zip([sent_status, received_status])
+          IO.puts("#{i}: #{inspect status}")
+        after 100 -> IO.puts("No reply from process #{i} in 100ms")
       end
-    end
-
-    listen(n_peers, 0)
-  end
-
-  def listen(num_of_peers, peer_counter) do
-    receive do
-      {:done, peer_index, sent_status, received_status} ->
-        status = List.zip([sent_status, received_status])
-        IO.puts("#{peer_index}: #{inspect status}")
-        #IO.inspect(status)
-        if peer_counter < num_of_peers - 1 do
-          listen(num_of_peers, peer_counter + 1)
-        end
     end
   end
 end
--- system1/lib/system6/peer.ex
+++ system1/lib/system6/peer.ex
@@ -1,21 +1,21 @@
 defmodule System6.Peer do
-  def run(system, peer_index, n_peers, max_broadcast) do
+  def run(system, peer_index, n_peers, max_broadcast, timeout) do
     peers = 0..n_peers-1
-    pl = spawn System4.LossyLink, :run, [peer_index]
-    beb = spawn BEB, :run, [peer_index, peers, pl]
-    rb = spawn RB, :run, [peer_index, peers, pl]
+    reliability = 50
+    pl = spawn LossyLink, :run, [reliability]
+    beb = spawn BEB, :run, [peers, pl]
+    rb = spawn RB, :run, [peers, beb]
     send system, {peer_index, pl}
     receive do
       {:ready} ->
-        initial_stats = List.duplicate(0, n_peers)
-        app = spawn System6.App, :run,
-          [peer_index, rb, initial_stats, initial_stats, max_broadcast]
+        app = spawn System6.App, :init,
+          [peer_index, system, rb, n_peers, max_broadcast, timeout]
         send rb, {:bind_app, app}
         send beb, {:bind_app, rb}
         send pl, {:bind_app, beb}
         if peer_index == 2 do
           receive do
-            after 5 -> 
+            after 5 ->
               Process.exit(app, :kill)
           end
         end
@@ -23,46 +23,35 @@ defmodule System6.Peer do
   end
 end
 
-defmodule RB do
-  def run(id, peers, beb, app \\ {}, delivered \\ []) do
-    receive do
-      {:bind_app, app} -> run(id, peers, beb, app)
-      {:rb_broadcast, msg} -> 
-        send beb, {:beb_broadcast, {msg, id}}
-        run(id, peers, beb, app)
-      {:beb_deliver, msg, from} -> 
-        if !Enum.member?(delivered, msg) do
-          send beb, {:beb_broadcast, msg}
-          send app, {:rb_deliver, msg, from} 
-          run(id, peers, beb, app, [msg | delivered])
-        else
-          run(id, peers, beb, app, delivered)
-        end
-    end
-  end
-end
-
 defmodule System6.App do
 
+  def init(id, system, rb, n_peers, max_broadcast, timeout) do
+    app_id = self()
+    spawn (fn -> receive do after timeout -> send app_id, {:stop} end end)
+
+    initial_stats = List.duplicate(0, n_peers)
+    run(id, system, rb, initial_stats, initial_stats, max_broadcast)
+  end
+
   # Max broadcasts reached
-  def run(id, rb, received_status, sent_status, 0) do
+  def run(id, system, rb, received_status, sent_status, 0) do
     receive do
-      {:rb_deliver, :stop, system} -> send system, {:done, id, sent_status, received_status}
-      {:rb_deliver, _, from} -> run(id, rb,
+      {:rb_deliver, {from, _}} -> run(id, system, rb,
         List.update_at(received_status, from, &(&1 + 1)), sent_status, 0)
+      {:stop} -> send system, {:done, id, sent_status, received_status}
     end
   end
 
-  def run(id, rb, received_status, sent_status, broadcast_left) do
+  def run(id, system, rb, received_status, sent_status, broadcast_left) do
     receive do
-      {:rb_deliver, :stop, system} -> send system, {:done, id, sent_status, received_status}
-      {:rb_deliver, msg, from} -> 
-        IO.puts("message is #{inspect msg}")
-        run(id, rb,
+      {:rb_deliver, {from, _}} -> run(id, system, rb,
         List.update_at(received_status, from, &(&1 + 1)), sent_status, broadcast_left)
+      {:stop} -> send system, {:done, id, sent_status, received_status}
       after 0 ->
-        send rb, {:rb_broadcast, broadcast_left}
-        run(id, rb, received_status, Enum.map(sent_status, &(&1 + 1)), broadcast_left - 1)
+        # Make sure the sent message is unique by including the number of
+        # broadcasts left for this process as well as it's unique id
+        send rb, {:rb_broadcast, {id, broadcast_left}}
+        run(id, system, rb, received_status, Enum.map(sent_status, &(&1 + 1)), broadcast_left - 1)
     end
   end
 end
