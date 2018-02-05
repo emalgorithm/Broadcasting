7d8b87a98f8325cef94b7f393312d172c3ab1cf3
Tencho Tenev <tt1215@imperial.ac.uk>
2018-02-05 18:10:47 +0000

    Add local timeout instead of system sending it

--- system1/lib/system2/peer.ex
+++ system1/lib/system2/peer.ex
@@ -1,12 +1,11 @@
 defmodule System2.Peer do
-  def run(system, peer_index, n_peers, max_broadcast) do
+  def run(system, peer_index, n_peers, max_broadcast, timeout) do
     pl = spawn PerfectLink, :run, [peer_index]
     send system, {peer_index, pl}
     receive do
       {:ready} ->
-        initial_stats = List.duplicate(0, n_peers)
-        app = spawn App, :run,
-          [peer_index, pl, 0..n_peers-1, initial_stats, initial_stats, max_broadcast]
+        app = spawn App, :init,
+          [peer_index, system, pl, n_peers, max_broadcast, timeout]
         send pl, {:bind_app, app}
     end
   end
@@ -28,26 +27,33 @@ defmodule PerfectLink do
 end
 
 defmodule App do
+  def init(id, system, pl, n_peers, max_broadcast, timeout) do
+    app_id = self()
+    spawn (fn -> receive do after timeout -> send app_id, {:stop} end end)
+
+    initial_stats = List.duplicate(0, n_peers)
+    run(id, system, pl, 0..n_peers-1, initial_stats, initial_stats, max_broadcast)
+  end
 
   # Max broadcasts reached
-  def run(id, pl, peers, received_status, sent_status, 0) do
+  def run(id, system, pl, peers, received_status, sent_status, 0) do
     receive do
-      {:pl_deliver, :stop, system} -> send system, {:done, id, sent_status, received_status}
-      {:pl_deliver, _, from} -> run(id, pl, peers,
+      {:pl_deliver, _, from} -> run(id, system, pl, peers,
         List.update_at(received_status, from, &(&1 + 1)), sent_status, 0)
+      {:stop} -> send system, {:done, id, sent_status, received_status}
     end
   end
 
-  def run(id, pl, peers, received_status, sent_status, broadcast_left) do
+  def run(id, system, pl, peers, received_status, sent_status, broadcast_left) do
     receive do
-      {:pl_deliver, :stop, system} -> send system, {:done, id, sent_status, received_status}
-      {:pl_deliver, _, from} -> run(id, pl, peers,
+      {:pl_deliver, _, from} -> run(id, system, pl, peers,
         List.update_at(received_status, from, &(&1 + 1)), sent_status, broadcast_left)
+      {:stop} -> send system, {:done, id, sent_status, received_status}
       after 0 ->
         for p <- peers do
           send pl, {:pl_send, 0, p}
         end
-        run(id, pl, peers, received_status, Enum.map(sent_status, &(&1 + 1)), broadcast_left - 1)
+        run(id, system, pl, peers, received_status, Enum.map(sent_status, &(&1 + 1)), broadcast_left - 1)
     end
   end
 end
